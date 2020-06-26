71ca172db34ebc076e10df86b42846da2931b8ee
Mahbuba Nimme <mahbuba102@gmail.com>
2020-06-26 16:12:02 -0400

    Intial Commit

---
+++ src/com/talentech/exmples/BasicJava.java
@@ -0,0 +1,9 @@
+package com.talentech.exmples;
+
+public class BasicJava {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
---
+++ src/com/talentech/exmples/BasicJava2.java
@@ -0,0 +1,9 @@
+package com.talentech.exmples;
+
+public class BasicJava2 {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
